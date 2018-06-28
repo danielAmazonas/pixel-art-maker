110bace8cb5ad17482dd9c2b5a6624b6e6f31f55
Daniel Souza Amazonas <daniel.amazonas@gmail.com>
2018-06-28 15:32:54 -0300

    docs: Adicionado comentários

--- js/designs.js
+++ js/designs.js
@@ -3,10 +3,13 @@
 
 // When size is submitted by the user, call makeGrid()
 
-let alturaGrid = $('#inputHeight').val();
-let larguraGrid = $('#inputWeight').val();
-let cor = '#000000';
+let alturaGrid = $('#inputHeight').val(); // altura inicial
+let larguraGrid = $('#inputWeight').val(); // largura inicial
+let cor = '#000000'; // cor inicial
 
+/**
+ * @description Funçao criar tabela
+ */
 function makeGrid() {
     $('#pixelCanvas').children().remove('tr');
     for (let x = 0; x < alturaGrid; x++) {
@@ -17,23 +20,38 @@ function makeGrid() {
     }
 }
 
+/**
+ * @description Pegar o valor da altura
+ */
 $('#inputHeight').focusout(function() {
     alturaGrid = $(this).val();
 });
 
+/**
+ * @description Pegar o valor da largura
+ */
 $('#inputWeight').focusout(function() {
     larguraGrid = $(this).val();
 });
 
+/**
+ * @description Ação ao botão para gerar a tabela
+ */
 $('#sizePicker').submit(function() {
     makeGrid();
     return false;
 });
 
+/**
+ * @description Ação de setar a cor
+ */
 document.getElementById('colorPicker').addEventListener('input', function() {
     cor = $(this).val();
 }, false);
 
+/**
+ * @description Ação de pintar a tabela com a cor definida
+ */
 $('#pixelCanvas').on('click', function(e) {
     $(e.target).css('background-color', cor);
 });
